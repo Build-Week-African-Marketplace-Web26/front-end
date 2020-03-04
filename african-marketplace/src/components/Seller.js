35ce1ecda9830c929124960041c50b9fac1f4add
Idong Essien <idong.essien@comcast.net>
2020-03-04 00:29:45 -0500

    Styled Seller and User.js components

--- african-marketplace/src/components/Seller.js
+++ african-marketplace/src/components/Seller.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ValidatedSignUpForm from './ValidatedSignUpForm';
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import FormikLoginForm from './ValidatedLoginForm.js';
+import styled from 'styled-components';
 
-
-
+const SellOrBuyContainer = styled.div`
+    background: rgba(0,0,0,0.8);
+    position: absolute;
+    z-index: 98;
+    width: 100%;
+`
 
 export const Seller = () => {
 
     return(
         <Router>
-            <div>                
+            <SellOrBuyContainer>                
                 <div className ="navDiv">         
                     <Link className ="user-link" to = '/signup'>Signup</Link>          
                     <Link  className ="user-link" to = '/signin'>Login</Link>         
@@ -20,8 +25,7 @@ export const Seller = () => {
                 <Route path ='/signup' component ={ValidatedSignUpForm} />
                 <Route path ='/signin' component ={FormikLoginForm}/>
                 </Switch>  
-                
-            </div>
+            </SellOrBuyContainer>
         </Router>
     )
 }
--- african-marketplace/src/components/User.js
+++ african-marketplace/src/components/User.js
@@ -2,15 +2,21 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import ValidatedSignUpForm from './ValidatedSignUpForm';
 import FormikLoginForm from './ValidatedLoginForm';
+import styled from 'styled-components';
 
-
+const SellOrBuyContainer = styled.div`
+    background: rgba(0,0,0,0.8);
+    position: absolute;
+    z-index: 98;
+    width: 100%;
+`
 
 export const User = () => {
 
 
     return(
         <Router>
-            <div>
+            <SellOrBuyContainer>
                 <div className="navDiv">      
                     <Link className ="user-link" to = '/signup'>Signup</Link>          
                     <Link className ="user-link" to = '/signin'>Login</Link>             
@@ -19,9 +25,8 @@ export const User = () => {
                 <Switch>
                     <Route path ='/signup' component ={ValidatedSignUpForm} />
                     <Route path ='/signin' component ={FormikLoginForm}/>
-                </Switch> 
-                              
-            </div>
+                </Switch>  
+            </SellOrBuyContainer>
         </Router>
     )
 }
